fead17111c6afb8380e2c9f43eda1232070f820b
milan8411 <milangadhvi8411@gmail.com>
2023-04-21 22:03:45 +0530

    auth

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeyzbvw4KMXsq7CIzdxAe1N-PTiSjoXLQ",
-  authDomain: "netflix-clone-851ae.firebaseapp.com",
-  projectId: "netflix-clone-851ae",
-  storageBucket: "netflix-clone-851ae.appspot.com",
-  messagingSenderId: "94738161392",
-  appId: "1:94738161392:web:ae1411630402fb7c99ac35",
-};
+    apiKey: "AIzaSyAq6Eqy1bYJM8dbm3VjfEekMoEzZ3TFyos",
+    authDomain: "netflix-clone-yt-5a0de.firebaseapp.com",
+    projectId: "netflix-clone-yt-5a0de",
+    storageBucket: "netflix-clone-yt-5a0de.appspot.com",
+    messagingSenderId: "35521071014",
+    appId: "1:35521071014:web:0344b29a3cdaec48bef167"
+  };    
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
